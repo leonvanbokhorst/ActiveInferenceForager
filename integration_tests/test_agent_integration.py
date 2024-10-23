157673afb7f9ac727eb874ba4afb5955ed8761d6
Leon van Bokhorst <post@leonvanbokhorst.com>
2024-10-23 09:19:56 +0200

    final-commit

--- integration_tests/test_agent_integration.py
+++ integration_tests/test_agent_integration.py
@@ -2,11 +2,14 @@ import unittest
 import logging
 from mas_dynamics_simulation.agent import Agent, Action
 from mas_dynamics_simulation.language_model.ollama_model import OllamaModel
-from mas_dynamics_simulation.language_model.language_model_handler import LanguageModelHandler
+from mas_dynamics_simulation.language_model.language_model_handler import (
+    LanguageModelHandler,
+)
 from mas_dynamics_simulation.decision_making import DecisionEngine
 from mas_dynamics_simulation.personality import BigFivePersonality, Personality
 from mas_dynamics_simulation.environment import Environment
 from typing import List, Dict
+
 
 class MockDecisionEngine(DecisionEngine):
     def evaluate_options(self, agent, options, environment):
@@ -14,6 +17,7 @@ class MockDecisionEngine(DecisionEngine):
 
     def __str__(self):
         return "MockDecisionEngine"
+
 
 class MockEnvironment(Environment):
     def get_state(self):
@@ -25,10 +29,19 @@ class MockEnvironment(Environment):
     def update(self):
         pass
 
+
 class ConcreteAgent(Agent):
     @classmethod
-    def generate_agent_details(cls, expertise: List[str], personality: Personality, decision_engine: "DecisionEngine", language_model_handler: LanguageModelHandler) -> Dict[str, str]:
-        return super().generate_agent_details(expertise, personality, decision_engine, language_model_handler)
+    def generate_agent_details(
+        cls,
+        expertise: List[str],
+        personality: Personality,
+        decision_engine: "DecisionEngine",
+        language_model_handler: LanguageModelHandler,
+    ) -> Dict[str, str]:
+        return super().generate_agent_details(
+            expertise, personality, decision_engine, language_model_handler
+        )
 
     def perceive(self, environment: Environment):
         return {}
@@ -45,6 +58,7 @@ class ConcreteAgent(Agent):
     def __str__(self):
         return f"ConcreteAgent: {self.name}"
 
+
 class Action:
     def __init__(self):
         self.name = "MockAction"
@@ -55,24 +69,31 @@ class Action:
     def __str__(self):
         return self.name
 
+
 class TestAgentIntegration(unittest.TestCase):
     def setUp(self):
         # Set up logging
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.basicConfig(
+            level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+        )
         self.logger = logging.getLogger(__name__)
 
         self.model_params = {
-            'base_url': 'http://localhost:11434',
-            'model_name': 'mistral-nemo'
+            "base_url": "http://localhost:11434",
+            "model_name": "llama3.2:latest",  # 'mistral-nemo' or 'llama3.2'
         }
         self.language_model_handler = LanguageModelHandler()
         self.language_model_handler.initialize(OllamaModel, self.model_params)
 
     def test_agent_initialization(self):
-        expertise = ["Project Management", "Leadership", "Teamwork", "Communication", "Problem Solving", "Adaptability", "Innovation", "Critical Thinking"]
+        expertise = [
+            "Positive Psychology",
+        ]
         decision_engine = MockDecisionEngine()
         personality = BigFivePersonality.random()
-        agent = ConcreteAgent(expertise, decision_engine, self.language_model_handler, personality)
+        agent = ConcreteAgent(
+            expertise, decision_engine, self.language_model_handler, personality
+        )
 
         self.assertIsNotNone(agent.name)
         self.assertIsNotNone(agent.backstory)
@@ -81,12 +102,22 @@ class TestAgentIntegration(unittest.TestCase):
         self.assertEqual(agent.expertise, tuple(expertise))
         self.assertIsInstance(agent.decision_engine, MockDecisionEngine)
         self.assertIsInstance(agent.personality, BigFivePersonality)
-        
+
         self.logger.info(f"Agent Name: {agent.name}")
         self.logger.info(f"Agent Backstory: {agent.backstory}")
         self.logger.info(f"Agent Bio: {agent.bio}")
         self.logger.info(f"Agent Dark Secret: {agent.dark_secret}")
+        self.logger.info(f"Agent Hobbies: {agent.hobbies}")
+        self.logger.info(f"Agent Interests: {agent.interests}")
+        self.logger.info(f"Agent Goals: {agent.goals}")
+        self.logger.info(f"Agent Fears: {agent.fears}")
+        self.logger.info(f"Agent Strengths: {agent.strengths}")
+        self.logger.info(f"Agent Weaknesses: {agent.weaknesses}")
+        self.logger.info(f"Agent Quirks: {agent.quirks}")
+        self.logger.info(f"Agent Public Behavior: {agent.public_behavior}")
+        self.logger.info(f"Agent Private Behavior: {agent.private_behavior}")
+        self.logger.info(f"Agent Health Issues: {agent.health_issues}") 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
